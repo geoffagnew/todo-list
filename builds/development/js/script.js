8e9f5e79fb14b2816116bc2e5c9731dfeb617e83
Geoff Agnew <gagnew@gmail.com>
2018-01-01 22:34:21 -0500

    Added logic and functionality for the clear all button.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -29,6 +29,7 @@ var sortable = require("sortablejs");
 var theList = $("#todo-list");
 var theInput = $("#toDoItem");
 var hasAlert = false;
+var clearBtn = $("#clear-all");
 
 function checkListInput() {
   var inputValLength = theInput.val().length;
@@ -42,24 +43,38 @@ function buildListItem() {
   theInput.val("");
 }
 
+// click event for adding new items to the list
 var addItemEvent = $("#addToDo").click(function (e) {
+
   e.preventDefault();
+  var listLength = $(theList).children().length;
+
   if (checkListInput() > 0 && hasAlert === true) {
     $("#input-alert").remove();
     buildListItem();
+    clearBtn.removeClass("hide");
     hasAlert = false;
   } else if (checkListInput() === 0 && hasAlert === true) {
     hasAlert = true;
     $("#input-alert").text("Sorry mate, try again.");
   } else if (checkListInput() > 0) {
     buildListItem();
+    clearBtn.removeClass("hide");
   } else {
     hasAlert = true;
     $("#listForm").append("<p id=input-alert>Please add your todo to the input field above.</p>");
   }
 });
 
+// click event for clear all list
+var clearList = $(clearBtn).click(function (e) {
+  e.preventDefault();
+  clearBtn.addClass("hide");
+  theList.children().remove();
+});
+
 exports.addItemEvent = addItemEvent;
+exports.clearList = clearList;
 
 },{"jquery":3,"sortablejs":4}],3:[function(require,module,exports){
 /*!
--- components/scripts/sort.js
+++ components/scripts/sort.js
@@ -9,35 +9,51 @@ const sortable = require("sortablejs");
 var theList = $("#todo-list");
 var theInput = $("#toDoItem");
 var hasAlert = false;
+const clearBtn = $("#clear-all");
 
-function checkListInput(){
+function checkListInput() {
   var inputValLength = theInput.val().length;
   return inputValLength;
 }
 
 function buildListItem() {
   var inputText = $(theInput).val();
-  var buildItem = "<li>" + inputText + "</li>";
+  var buildItem = `<li>${inputText}</li>`;
   theList.append(buildItem);
   theInput.val("");
 }
 
+// click event for adding new items to the list
 var addItemEvent = $("#addToDo").click(function(e){
+
   e.preventDefault();
+  var listLength = $(theList).children().length;
+
   if (checkListInput() > 0 && hasAlert === true) {
     $("#input-alert").remove();
     buildListItem();
+    clearBtn.removeClass("hide");
     hasAlert = false;
   } else if (checkListInput() === 0 && hasAlert === true) {
     hasAlert = true;
     $("#input-alert").text("Sorry mate, try again.");
   } else if (checkListInput() > 0) {
     buildListItem();
+    clearBtn.removeClass("hide");
   } else {
     hasAlert = true;
     $("#listForm").append("<p id=input-alert>Please add your todo to the input field above.</p>");
   }
+
+});
+
+// click event for clear all list
+var clearList = $(clearBtn).click(function(e) {
+  e.preventDefault();
+  clearBtn.addClass("hide");
+  theList.children().remove();
 });
 
 exports.addItemEvent = addItemEvent;
+exports.clearList = clearList;
 
