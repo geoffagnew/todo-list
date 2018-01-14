88a8208ae61834de77eeba83b03eaac7df790c8c
Geoff Agnew <gagnew@gmail.com>
2018-01-13 22:46:42 -0500

    Fixed cursor placement issue in FF by restricting sortability to the .handle element alone.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -6,7 +6,8 @@
 var $ = require("jquery");
 
 // -------------------------- Plugins
-var sortable = require("sortablejs"); // plugin for html5 drag and drop sorting
+// plugin for html5 drag and drop sorting
+var sortable = require("sortablejs");
 // load dom purify sanitizer
 var DOMPurify = require("dompurify");
 
@@ -19,6 +20,7 @@ var hasAlert = false;
 
 // initiate the sortable plugin
 var sortIt = sortable.create(sortContainer, {
+  handle: ".handle",
   onEnd: function onEnd(evt) {
     localStorage.setItem("savedList", theList.html());
   }
@@ -40,14 +42,13 @@ function sanitizeInput(val) {
 function buildListItem() {
   var inputText = $(theInput).val();
   var cleanText = sanitizeInput(inputText);
-  var buildItem = "<li><span class=\"handle\">::</span><span class=\"item-content\">" + cleanText + "</span><span class=\"removeListItem\">x</span></li>";
+  var buildItem = "<li><span class=\"handle\">::</span><input class=\"item-content\" type=\"text\" value=\"" + cleanText + "\"><span class=\"removeListItem\">x</span></li>";
   theList.append(buildItem);
   theInput.val("");
 }
 
 // click event for adding new items to the list
 $("#addToDo").click(function (e) {
-
   e.preventDefault();
   var listLength = $(theList).children().length;
 
@@ -78,45 +79,17 @@ $(theList).click(function (e) {
   }
 });
 
-// listener for editing a list item
-$(theList).click(function (e) {
+// listener event for the editing list item inline
+$(theList).focusout(function (e) {
   e.preventDefault();
-  // get the clicked element
   var target = $(e.target);
-  // if the element clicked was span.item-content
-  if (target.is("span.item-content")) {
-    target.parent().addClass("active-edit");
-    // get the original text from the list item
-    var origText = target.text();
-    // create a temporary input field to show while user is editing item
-    var tempInput = "<input class=\"item-content\" type=\"text\" value=\"" + origText + "\">";
-    // temporarily replace span.item-content with tempInput
-    target.replaceWith(tempInput);
-    // focus cursor to beginning of input
-    $(".item-content").focus();
-    // fire focus out event listener
-    $(".item-content").focusout(function (e) {
-      var newVal = $(this).val();
-      var cleanText = sanitizeInput(newVal);
-      var newOutput = "<span class=\"item-content\">" + cleanText + "</span>";
-      $(this).parent().removeClass("active-edit");
-      $(this).replaceWith(newOutput);
-      localStorage.setItem("savedList", theList.html());
-    });
+  var currentVal = target.val();
+  var cleanText = sanitizeInput(currentVal);
+  if (target.is("input")) {
+    target.attr("value", cleanText);
+    localStorage.setItem("savedList", theList.html());
   }
 });
-
-// listener event for the editing list item inline
-// $(theList).focusout(function(e) {
-//   e.preventDefault();
-//   var target = $(e.target);
-//   var currentVal = target.val();
-//   var cleanText = sanitizeInput(currentVal);
-//   if (target.is("input")) {
-//     target.attr("value", cleanText);
-//     localStorage.setItem("savedList", theList.html());
-//   }
-// });
 
 // click event for the saveall button
 $("#save-all").on("click", function (e) {
